be871458c0f5ead8dee186d10ddf48f167bac347
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-11-12 15:24:08 -0600

    Update SRRmerge.pl

--- bin/SRRmerge.pl
+++ bin/SRRmerge.pl
@@ -12,8 +12,34 @@ use warnings;
 use Cwd;
 
 my $file=shift @ARGV;
-my %SRA;
+my %SRR;
+my %SRX;
 open F,$file;
 while(<F>){
 chomp;
+my @line=split/\s+/;
+push @{$SRX{$line[2]}}, $line[0];
+print "$line[0]\t$line[2]\n";
+}
+
+foreach my $srx(sort keys %SRX){
+my @SRR=@{$SRX{$srx}};
+my %beta;
+foreach my $srr(@SRR){
+print "$srr\n";
+open F,"$srr\_1_val_1_bismark_bt2_pe.nonCG_filtered.bismark.cov" || die "cannot find $srr";
+while(<F>){
+my @line=split/\s+/;
+my $pos="$line[0]:$line[1]-$line[2]";
+$beta{$pos}{"M"}+=$line[4];
+$beta{$pos}{"U"}+=$line[5];
+}
+}
+open OUT,">$srx.bedgraph";
+foreach my $pos(sort keys %beta){
+my $beta=$beta{$pos}{"M"}/($beta{$pos}{"M"}+$beta{$pos}{"U"});
+my ($CHR,$START,$END)=split/:|-/,$pos;
+print OUT "$CHR\t$START\t$END\t$beta\t$beta{$pos}{'M'}\t$beta{$pos}{'U'}\n";
+}
+close OUT;
 }
