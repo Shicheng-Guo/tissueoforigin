ff9664a646598992ba16a159e4e69da74a10070f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-11-13 01:58:40 -0600

    Update DSS.R

--- bin/DSS.R
+++ bin/DSS.R
@@ -43,13 +43,21 @@ for(file in filenames)
   input = append(input,list(read_bismarkcov(file)))
 }
 save(input,file="input.RData")
+
 #Calling differentially methylated sites
 BSobj <- makeBSseqData(input,c('LYMP-S2019-0003-1','LYMP-S2019-0004-1','LYMP-S2019-0003-2','LYMP-S2019-0007-2') )
 save(BSobj,file="BSobj.RData")
 dmlTest <- DMLtest(BSobj, group1=c('LYMP-S2019-0003-1','LYMP-S2019-0004-1'), group2=c('LYMP-S2019-0003-2','LYMP-S2019-0007-2'),smoothing = T, smoothing.span = 200)
 save(dmlTest,file="dmlTest.region.RData")
-DMS = callDML(dmlTest)
+write.table(dmlTest,file="output.LYMP.DMC.tsv",row.names=F,quote=F,sep="\t")
+
 #Write output to hard disk
+DMS = callDML(dmlTest)
 fdr_cutoff=0.05
 index = which(DMS$fdr <= fdr_cutoff)
 write.table(DMS[index,],file="output.LYMP.DMR.tsv",row.names=F,quote=F,sep="\t")
+
+# Manhattan plot and qqplot
+
+
+
