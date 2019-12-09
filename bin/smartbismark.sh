8c9e0555ffbd3d5c707a8da42df28e8a5a7a6abe
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-12-08 19:39:59 -0600

    Update smartbismark.sh

--- bin/smartbismark.sh
+++ bin/smartbismark.sh
@@ -1,8 +1,10 @@
-perl smartbismark.pl --input SraRunTable.txt --genome hg19 --server MCRI --queue shortq --submit submit
-perl smartbismark.pl --input srr3.txt --genome hg19 --server MCRI --queue shortq --submit submit
-perl smartbismark.pl --input srr4.txt --genome hg19 --server MCRI --queue shortq --submit submit
-perl smartbismark.pl --input srr4.txt --genome hg19 --server MCRI --queue shortq --submit no
-cp /gpfs/home/guosa/hpc/methylation/clep/wgbs/smartbismark.pl /gpfs/home/guosa/PRJNA577192/srr
-perl smartbismark.pl --input SraRunTable2.txt --genome hg19 --server MCRI --queue shortq --submit submit
+# perl smartbismark.pl --input SraRunTable.txt --genome hg19 --server MCRI --queue shortq --submit submit
+# perl smartbismark.pl --input srr3.txt --genome hg19 --server MCRI --queue shortq --submit submit
+# perl smartbismark.pl --input srr4.txt --genome hg19 --server MCRI --queue shortq --submit submit
+# perl smartbismark.pl --input srr4.txt --genome hg19 --server MCRI --queue shortq --submit no
+# cp /gpfs/home/guosa/hpc/methylation/clep/wgbs/smartbismark.pl /gpfs/home/guosa/PRJNA577192/srr
+# perl smartbismark.pl --input SraRunTable2.txt --genome hg19 --server MCRI --queue shortq --submit submit
+# perl smartbismark.pl --input config2.txt --genome hg19 --server MCRI --queue shortq --submit submit
+
 perl smartbismark.pl --input config2.txt --genome hg19 --server MCRI --queue shortq --submit submit
 
